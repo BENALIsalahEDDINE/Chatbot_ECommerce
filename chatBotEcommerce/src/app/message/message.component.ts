d86d23df5c9193a2ac5f591d1cb5041edfbf148d
BENALIsalahEDDINE <salaheddinebenali@student.emi.ac.ma>
2021-10-19 19:25:17 +0100

    V1.0

--- chatBotEcommerce/src/app/message/message.component.ts
+++ chatBotEcommerce/src/app/message/message.component.ts
@@ -6,6 +6,7 @@ import { Component, Input ,OnInit } from '@angular/core';
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
+  @Input('text') text : string;
   @Input('owner') owner: boolean;
   @Input('colorBackRight') colorBackRight: string;
   @Input('colorFontRight') colorFontRight: string;
