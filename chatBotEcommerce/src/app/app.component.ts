68fa99abd1a95cac89e20f6534c2355f56cf037c
BENALIsalahEDDINE <salaheddinebenali@student.emi.ac.ma>
2021-10-19 19:03:45 +0100

    V1.0

--- chatBotEcommerce/src/app/app.component.ts
+++ chatBotEcommerce/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'chatBotEcommerce';
+  colorBackRight: string = '#007bff';
+  colorFontRight: string = '#ffffff';
+  message =[];
 }
